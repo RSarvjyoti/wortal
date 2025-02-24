06478518cfbdd2a1b11c1bb805edc6f3070db9d1
RSarvjyoti <sarvjyoti64@gmail.com>
2025-02-24 20:13:02 +0530

    added backend deploed api

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api"; 
+const API_URL = "https://wortal-1.onrender.com"
 
 export const registerUser = async (userData) => {
   try {
